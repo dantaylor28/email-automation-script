37d7ed49646a0930a2e6ede31b05c66a14f0e5cd
Dan <dan.taylor1493@gmail.com>
2025-05-19 22:09:30 +0200

    Define a function to find all links in emails html content that contain the word unsubscribe

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import imaplib
 import email
 import os
+from bs4 import BeautifulSoup
 load_dotenv()
 
 username = os.getenv("EMAIL")
@@ -13,27 +14,10 @@ def connect_to_mail():
     mail.select("inbox")
     return mail
 
-# def search_for_email():
-#     mail = connect_to_mail()
-#     _, search_data = mail.search(None, '(BODY "unsubscribe")')
-#     data = search_data[0].split()
-
-#     for num in data:
-#         _, data = mail.fetch(num, "(RFC822)")
-#         msg = email.message_from_bytes(data[0][1])
-
-#         if msg.is_multipart():
-#             for part in msg.walk():
-#                 if part.get_content_type() == "text/html":
-#                     html_content = part.get_payload(decode=True).decode()
-#                     print(html_content)
-#         else:
-#             content_type = msg.get_content_type()
-#             content = msg.get_payload(decode=True).decode()
-
-#             if content_type == "text/html":
-#                 print(content)
-#     mail.logout()
+def extract_links_from_html(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    links = [link["href"] for link in soup.find_all("a", href=True) if "unsubscribe" in link["href"].lower()]
+    return links
 
 def search_for_email():
     mail = connect_to_mail()
